ff4d14905a90e71d591d85a6ea899ce9181eae86
Ivan Alenin <aisproect@yandex.ru>
2020-12-17 13:34:58 +0300

    Config for GH pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,12 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
+  target:'static',
+  router: {
+    base: '/nuxt-shop/'
+  },
+  generate:{
+    dir:'build'
+  },
   head: {
     title: 'shop',
     meta: [
